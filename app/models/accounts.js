6859437c23a75ceaadb19c507044abfae5149de3
EgidioCaprinoCloudreach <egidio.caprino@cloudreach.co.uk>
2016-06-14 10:46:27 +0100

    Account and service datas

---
+++ app/models/accounts.js
@@ -0,0 +1,3 @@
+app.factory('accountsData', function () {
+    return [{}];
+});
---
+++ app/models/services.js
@@ -0,0 +1,14 @@
+app.factory('servicesData', function () {
+    return [
+        {
+            id: 1,
+            name: 'Dropbox',
+            icon: 'http://www.iconarchive.com/download/i94306/bokehlicia/captiva/dropbox.ico'
+        },
+        {
+            id: 2,
+            name: 'Google Drive',
+            icon: 'https://skyvia.com/connectors/images/google-drive.png'
+        }
+    ];
+});
